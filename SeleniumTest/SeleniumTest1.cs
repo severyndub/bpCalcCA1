3342cb405032dc13dad69db72f7955b8d23a53b9
Test User <severyndub@gmail.com>
2018-11-09 06:54:09 +0000

    changing back to chrome adding more tests

--- SeleniumTest/SeleniumTest1.cs
+++ SeleniumTest/SeleniumTest1.cs
@@ -19,7 +19,25 @@ namespace SeleniumTest
         { }
 
         [TestMethod]
-        [TestCategory("Firefox")]
+        [TestCategory("Chrome")]
+        public void HighPressureTest()
+        {
+            ChromeOptions test = new ChromeOptions();
+            test.AddArgument("no - sandbox");
+            driver.Navigate().GoToUrl(appURL + "/BloodPressure");
+            //Clear the input fields
+            driver.FindElement(By.Id("BP_Systolic")).Clear();
+            //insert some test data
+            driver.FindElement(By.Id("BP_Systolic")).SendKeys(systolicHigh);
+            driver.FindElement(By.Id("BP_Diastolic")).Clear();
+            driver.FindElement(By.Id("BP_Diastolic")).SendKeys(diastolicHigh);
+            driver.FindElement(By.Id("BP_Systolic")).Click();
+            string resultText = driver.FindElement(By.Id("bpCategoryResult")).Text;
+            Assert.IsTrue(resultText.Equals("High Blood Pressure"));
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
         public void PreHighTest()
         {
             ChromeOptions test = new ChromeOptions();
@@ -28,13 +46,51 @@ namespace SeleniumTest
             //Clear the input fields
             driver.FindElement(By.Id("BP_Systolic")).Clear();
             //insert some test data
-            driver.FindElement(By.Id("BP_Systolic")).SendKeys("120");
+            driver.FindElement(By.Id("BP_Systolic")).SendKeys(systolicPreHigh);
             driver.FindElement(By.Id("BP_Diastolic")).Clear();
-            driver.FindElement(By.Id("BP_Diastolic")).SendKeys("80");
+            driver.FindElement(By.Id("BP_Diastolic")).SendKeys(diastolicPreHigh);
             driver.FindElement(By.Id("BP_Systolic")).Click();
             string resultText = driver.FindElement(By.Id("bpCategoryResult")).Text;           
             Assert.IsTrue(resultText.Equals("Pre-High Blood Pressure"));
+            
         }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void NormalPressureTest()
+        {
+            ChromeOptions test = new ChromeOptions();
+            test.AddArgument("no - sandbox");
+            driver.Navigate().GoToUrl(appURL + "/BloodPressure");
+            //Clear the input fields
+            driver.FindElement(By.Id("BP_Systolic")).Clear();
+            //insert some test data
+            driver.FindElement(By.Id("BP_Systolic")).SendKeys(systolicNormal);
+            driver.FindElement(By.Id("BP_Diastolic")).Clear();
+            driver.FindElement(By.Id("BP_Diastolic")).SendKeys(diastolicNormal);
+            driver.FindElement(By.Id("BP_Systolic")).Click();
+            string resultText = driver.FindElement(By.Id("bpCategoryResult")).Text;
+            Assert.IsTrue(resultText.Equals("Normal Blood Pressure"));
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void LowPressureTest()
+        {
+            ChromeOptions test = new ChromeOptions();
+            test.AddArgument("no - sandbox");
+            driver.Navigate().GoToUrl(appURL + "/BloodPressure");
+            //Clear the input fields
+            driver.FindElement(By.Id("BP_Systolic")).Clear();
+            //insert some test data
+            driver.FindElement(By.Id("BP_Systolic")).SendKeys(systolicLow);
+            driver.FindElement(By.Id("BP_Diastolic")).Clear();
+            driver.FindElement(By.Id("BP_Diastolic")).SendKeys(diastolicLow);
+            driver.FindElement(By.Id("BP_Systolic")).Click();
+            string resultText = driver.FindElement(By.Id("bpCategoryResult")).Text;
+            Assert.IsTrue(resultText.Equals("Low Blood Pressure"));
+        }
+
 
         /// <summary>
         ///Gets or sets the test context which provides
@@ -56,7 +112,7 @@ namespace SeleniumTest
         public void SetupTest()
         {
             appURL = "https://bpcalctest-bpcalcqa.azurewebsites.net/";
-            string browser = "Firefox";
+            string browser = "Chrome";
             switch (browser)
             {
                 case "Chrome":
@@ -81,5 +137,21 @@ namespace SeleniumTest
         {
             driver.Quit();
         }
+
+        //High pressure test parametrs
+        private const String systolicHigh = "160";
+        private const String diastolicHigh = "100";
+
+        // Pre High pressure test parameters
+        private const String systolicPreHigh = "120";
+        private const String diastolicPreHigh = "80";
+
+        //Normal pressure test parametrs
+        private const String systolicNormal = "100";
+        private const String diastolicNormal = "80";
+
+        //Normal pressure test parametrs
+        private const String systolicLow = "80";
+        private const String diastolicLow = "60";
     }
 }
